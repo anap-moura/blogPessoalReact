dc2113b0c0e5af18fed1667eec89b855514fee23
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-24 12:17:17 -0300

    login e cadastro ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
+
 import './App.css';
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
-          <Footer />
+          <Footer/>
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
