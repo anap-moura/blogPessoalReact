f0d6a99f1b3a1a5c40e5c2b9b10e2b4293cd27e6
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-29 18:35:47 -0300

    crud postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,36 @@
-import { useContext } from 'react';
-
-import { UserContext } from '../../contexts/UserContext';
-import { Link } from 'react-router-dom';
+import './Home.css';
+import homeLogo from '../../assets/home.svg'
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 function Home() {
-  const { nome} = useContext(UserContext);
-
-  return (
-    <div className='flex justify-center items-center'>
-      <div>
-        <h2 className="text-slate-900 text-5xl  my-4">Logar</h2>
-        <h2 className="text-slate-900 text-4xl ">Ola user : {nome}</h2>
-        <Link to="/login" className="my-4 rounded bg-pink-200
-         hover:bg-pink-400 text-white w-1/2 py-2 flex justify-center">
-          Voltar 
-        </Link>
-      </div>
-
-    </div>
-  );
+    return (
+        <>
+        <div className="bg-pink-400 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              <ModalPostagem />
+                <button className='rounded bg-white text-pink-400 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+            <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
+          </div>
+        </div>
+        <ListaPostagens />
+      </>
+    );
 }
 
 export default Home;
+
+
+
+
